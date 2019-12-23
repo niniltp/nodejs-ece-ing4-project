56c1fd5a06832c0bef2ed69219bc8e8c5538944d
niniltp <nhienphan923@gmail.com>
2019-12-23 17:59:05 +0100

    rm mkdir

--- src/controllers/metrics.ts
+++ src/controllers/metrics.ts
@@ -2,8 +2,6 @@ import {Metric, MetricsHandler} from "../models/metrics";
 const config = require('../helpers/_config');
 
 const dbPath = process.env.NODE_ENV === 'test' ? config.dbPath['test'] : config.dbPath['development'];
-const {createFile} = require('../helpers/files');
-createFile(dbPath + '/metrics');
 const dbMet: MetricsHandler = new MetricsHandler(dbPath + '/metrics');
 
 /* Metric CRUD */
