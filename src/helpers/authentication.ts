3f963c5399e7f4d9d00e29427c6eb199b52d79c8
niniltp <nhienphan923@gmail.com>
2019-12-24 00:09:39 +0100

    change http code forbidden

--- src/helpers/authentication.ts
+++ src/helpers/authentication.ts
@@ -8,5 +8,5 @@ exports.authCheck = (req: any, res: any, next: any) => {
 exports.usernameCheck = (req: any, res: any, next: any) => {
     if (req.session.loggedIn && req.session.user && req.params.username && req.session.user.username === req.params.username) {
         next();
-    } else  res.status(404).send("You don't have the rights");
+    } else  res.status(403).send("You don't have the rights");
 };
